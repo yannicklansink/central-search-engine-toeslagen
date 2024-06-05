e7f041687c07bb3fb1d3415dc822c762fb9fd1bb
YannickLansink <YannickLansink@KBCBelastingdiensttrajecten.onmicrosoft.com>
2024-06-05 13:06:54 +0200

    added dotnet console app client to interact with fastapi server

---
+++ langserve-client-console-app/langserve-client-console-app/Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace langserve_client_console_app
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // De URL van je langchain langserve API
+            var apiUrl = "http://localhost:8000/chain/invoke";
+
+            // Maak een HTTP-client aan
+            using (var client = new HttpClient())
+            {
+                // Stel de gegevens in die je wilt versturen
+                var data = new
+                {
+                    input = new
+                    {
+                        language = "ffrench",
+                        text = "hi"
+                    }
+                };
+
+                // Converteer de gegevens naar een JSON-string
+                var json = JsonConvert.SerializeObject(data);
+
+                // Maak een HTTP-content object van de JSON-string
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                // Verstuur een POST-verzoek naar de API
+                var response = await client.PostAsync(apiUrl, content);
+
+                // Zorg dat je de response controleert
+                if (response.IsSuccessStatusCode)
+                {
+                    // Lees de inhoud van de response
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Response: " + responseContent);
+                }
+                else
+                {
+                    Console.WriteLine("Error: " + response.StatusCode);
+                }
+            }
+        }
+
+    }
+}
---
+++ rag-3/serve.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+from typing import List
+
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from langserve import add_routes
+from dotenv import load_dotenv
+import os
+
+# Laad de variabelen uit het .env bestand
+load_dotenv()
+
+# Gebruik de variabelen
+tracing = os.getenv('LANGCHAIN_TRACING_V2')
+endpoint = os.getenv('LANGCHAIN_ENDPOINT')
+api_key_langchain = os.getenv('LANGCHAIN_API_KEY')
+api_key_openai = os.getenv('OPENAI_API_KEY')
+
+
+
+# 1. Create prompt template
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
+
+# 2. Create model
+model = ChatOpenAI()
+
+# 3. Create parser
+parser = StrOutputParser()
+
+# 4. Create chain
+chain = prompt_template | model | parser
+
+
+# 4. App definition
+app = FastAPI(
+  title="LangChain Server",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
+)
+
+from fastapi.middleware.cors import CORSMiddleware
+
+# Set all CORS enabled origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+# 5. Adding chain route
+add_routes(
+    app,
+    chain,
+    path="/chain",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
